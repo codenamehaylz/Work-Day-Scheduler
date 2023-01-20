38fd1800365929316fb15c7087f90903739ecece
Hayley <hb_060492@hotmail.co.uk>
2023-01-20 22:06:22 +0000

    save button will save to local storage but not retrieve

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+localStorage.getItem("saved");
+
 var saveBtn = $('.saveBtn i');
 var currentTime = moment().hour();
 console.log(currentTime);
@@ -21,10 +23,21 @@ $('.time-block').each(function(){
 //function for saving text to local storage
 
 function saveEvent(event){
-    var targetRow = $(event.target).parent().parent();
-    var newText = targetRow.children('.description').children('textarea').val();
-    console.log(newText);
+    var newEvent = $(event.target).parent().siblings('.description').children('textarea').val();
+    var hourOfEvent = $(event.target).parent().parent().attr("id");
+    console.log(newEvent);
+    console.log(hourOfEvent);
+    var saved = {
+        hour: hourOfEvent,
+        text: newEvent
+    }
+    //TODO push to an empty array of objects??
+    localStorage.setItem("saved", JSON.stringify(saved));
+    JSON.parse(localStorage.getItem("saved"));
 }
+
+//TODO function for retrieving saved events??
+//TODO loop through for each row, if id === saved.hour
 
 //on click event for save button, calls save function
 saveBtn.on("click", saveEvent);
