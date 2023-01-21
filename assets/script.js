5f8d6b3e12c62d981d80585959b1ad4f0ba756f5
Hayley <hb_060492@hotmail.co.uk>
2023-01-21 17:30:24 +0000

    saving event now overwrites the previous event at that hour

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
-//TODO event should overwrite previous saved event
-
+//Function to check local storage for events and render onto page
 renderSavedEvents();
 
 function renderSavedEvents(){
     var savedEvents = JSON.parse(localStorage.getItem("saved"));
-    console.log(savedEvents);
     if (savedEvents !== null){
         $('.time-block').each(function(){
             var timeblockHour = (parseInt($(this).attr("id")));
             for (var i=0; i<savedEvents.length; i++){
-                
                 if (savedEvents[i].hour == timeblockHour){
                     $(this).children('.description').children('textarea').val(savedEvents[i].text);
                 }
@@ -18,14 +15,12 @@ function renderSavedEvents(){
     }
 }
 
-var saveBtn = $('.saveBtn i');
-var currentTime = moment().hour();
-
 //puts the current date in the header
 var today = moment();
 $('#currentDay').text(today.format("dddd, Do MMMM YYYY"));
 
 //targets all rows and checks the current time against each
+var currentTime = moment().hour();
 $('.time-block').each(function(){
     var timeblockHour = (parseInt($(this).attr("id")));
     if (timeblockHour === currentTime){
@@ -38,23 +33,24 @@ $('.time-block').each(function(){
 })
 
 //function for saving text to local storage
-
 function saveEvent(event){
     var eventText = $(event.target).parent().siblings('.description').children('textarea').val();
     var hourOfEvent = $(event.target).parent().parent().attr("id");
-    console.log(eventText);
-    console.log(hourOfEvent);
-
     var savedEvents = JSON.parse(localStorage.getItem("saved")) || [];
-    console.log('before', savedEvents);
     var saved = {
         hour: hourOfEvent,
         text: eventText
     }
+    //checks saved events for an event at the same hour, removes it
+    for (var i=0; i<savedEvents.length; i++){
+        if (savedEvents[i].hour == saved.hour){
+            savedEvents.splice(i, 1);
+            break;
+        }
+    }
     savedEvents.push(saved);
-    console.log('after', savedEvents);
     localStorage.setItem("saved", JSON.stringify(savedEvents));
 }
 
 //on click event for save button, calls save function
-saveBtn.on("click", saveEvent);
+$('.saveBtn i').on("click", saveEvent);
